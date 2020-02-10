43492541bbddecb8382efce48c16af8bd5515fc8
Katie Raby <katie.raby@icloud.com>
2020-02-10 17:34:26 +0000

    Progress with addNewItem logic, minor styling edits

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 const listItems = [...document.getElementsByClassName("list-item")];
-// const inputBox = document.getElementByClassName("input-box");
+const myItemList = document.getElementsByClassName("list")[0];
+const inputBox = document.getElementsByClassName("input-box")[0];
 
-// console.log(inputBox);
-// console.dir(inputBox);
-
-// what do you want to interact with ? my input box
-// how ?
-// what do you want to do as a result?
+console.dir(inputBox);
 
 function completed(event) {
   event.srcElement.classList.toggle("strikethrough");
   event.srcElement.children[0].toggleAttribute("checked");
 }
 
+function addNewItem(event) {
+  if (event.which === 13) {
+    const newLi = document.createElement("li");
+    newLi.classList.add("list-item");
+    //newLi.innerHTML
+    myItemList.appendChild(newLi);
+  }
+}
+
 listItems.forEach(item => item.addEventListener("click", completed));
+inputBox.addEventListener("keydown", addNewItem);
